b7973d7015dece238be66dc8139a612e53b8af14
Your Name <you@example.com>
2026-01-20 08:04:01 +0900

    現時点での最新版uiのサンプル起動用フロントエンドを追加

---
+++ source/app_frontend_proto/app_frontend_proto_v3/frontend_gcloud_command_sample.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# 改行コードは LF。
+
+set -e # 失敗したら止める
+
+# Settings
+# プロジェクトIDを環境変数にセット
+PROJECT_ID=$(gcloud config get-value project)
+# リージョン設定
+LOCATION=asia-northeast1
+# プロジェクト設定
+gcloud config set project $PROJECT_ID
+# リポジトリ名
+REPO_NAME=bidapp-frontend-proto-v3
+# イメージ名
+IMAGE_NAME=bidapp-frontend-proto-v3
+# タグ名
+TAG_NAME=v3
+
+# リポジトリ作成
+if ! gcloud artifacts repositories describe $REPO_NAME --location=$LOCATION >/dev/null 2>&1; then
+   gcloud artifacts repositories create $REPO_NAME \
+     --repository-format=docker \
+     --location=$LOCATION \
+     --description="${REPO_NAME} container repository"
+fi
+
+# Cloud Build → Artifact Registry
+# LOCATION-docker.pkg.dev/PROJECT_ID/REPOSITORY/IMAGE_NAME:TAG
+# (gcr.io/PROJECT_ID/REPOSITORY/IMAGE_NAME:TAG)
+gcloud builds submit --tag $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$REPO_NAME:$TAG_NAME
+
+# Cloud Run デプロイ
+gcloud run deploy $IMAGE_NAME \
+  --image $LOCATION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME:$TAG_NAME \
+  --region $LOCATION \
+  --allow-unauthenticated
