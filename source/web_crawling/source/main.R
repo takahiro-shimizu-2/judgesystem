afc09192861e8b135d49ebe560a332fcbbbdf3b1
Your Name <you@example.com>
2026-02-03 14:30:44 +0900

    公告判定のRのコード更新

--- source/web_crawling/source/main.R
+++ source/web_crawling/source/main.R
@@ -15,7 +15,9 @@ d1 <- read.table(
 table(d1$pdf_types)
 d2 <- d1[d1$pdf_types=="その他",]
 
-cl <- makeCluster( detectCores() %/% 2 )
+# cl <- makeCluster( detectCores() %/% 2 )
+
+cl <- makeCluster( detectCores() )
 clusterEvalQ(cl, library(pdftools))
 system.time({
   #result <- lapply(d2[["save_path"]], function(path) {
@@ -33,60 +35,72 @@ system.time({
     if(is.na(tex2)){
       return(NA)
     }
-    hed <- gsub(" ","", strsplit(tex2,"\n")[[1]][1])
-    tmp <- strsplit(tex2,"\n")[[1]]
-    tex3 <- tmp[1:min(c(length(tmp),20))]
+    tex3 <- strsplit(tex2,"\n")[[1]]
+    tex3 <- tex3[1:min(c(length(tex3),20))]
+    tex3 <- gsub(" ","",tex3)
+    
+    hed <- tex3[1]
     
     if(grepl("入札公告",hed)){
       return("入札公告")
     }
-
+    
     if(grepl("入札結果",hed)){
       return("入札結果")
     }
     
-    if(grepl("^公告",hed)){
-      return("公告")
-    }
-    
-    if(grepl("仕様書",hed)){
-      return("仕様書")
-    }
-    
-    if(any(tex3=="公告")){
-      return("公告")
-    }
-    
-    if(any(tex3=="変更公告")){
-      return("変更公告")
-    }
-    
-    if(any(grepl("情報・提案要求書",tex3))){
-      return("情報・提案要求書")
-    }
-    
-    if(grepl("競争参加者の資格に関する公示",hed)){
-      return("競争参加者の資格に関する公示")
-    }
-    
-    if(grepl("仕様書番号",hed)){
-      return("仕様書番号")
-    }
-    
-    if(grepl("公示.*号",hed)){
-      return("公示xx号")
-    }
-    
-    if(grepl("見積依頼",hed)){
-      return("見積依頼")
-    }
-    
-    if(grepl("^支担官第.*号",hed)){
-      return("支担官第xx号")
-    }
-    
-    if(any(grepl("業者の選定",tex3))){
-      return("業者の選定")
+    if(0){
+      if(grepl("入札公告",hed)){
+        return("入札公告")
+      }
+  
+      if(grepl("入札結果",hed)){
+        return("入札結果")
+      }
+      
+      if(grepl("^公告",hed)){
+        return("公告")
+      }
+      
+      if(grepl("仕様書",hed)){
+        return("仕様書")
+      }
+      
+      if(any(tex3=="公告")){
+        return("公告")
+      }
+      
+      if(any(tex3=="変更公告")){
+        return("変更公告")
+      }
+      
+      if(any(grepl("情報・提案要求書",tex3))){
+        return("情報・提案要求書")
+      }
+      
+      if(grepl("競争参加者の資格に関する公示",hed)){
+        return("競争参加者の資格に関する公示")
+      }
+      
+      if(grepl("仕様書番号",hed)){
+        return("仕様書番号")
+      }
+      
+      if(grepl("公示.*号",hed)){
+        return("公示xx号")
+      }
+      
+      if(grepl("見積依頼",hed)){
+        return("見積依頼")
+      }
+      
+      if(grepl("^支担官第.*号",hed)){
+        return("支担官第xx号")
+      }
+      
+      if(any(grepl("業者の選定",tex3))){
+        return("業者の選定")
+      }
     }
     
     return(hed)
@@ -98,97 +112,97 @@ d1[["pdf_types_2"]] <- unlist(result)
 
 
 
-
-pdfurl_subset_cols <- c("pdfUrl","pdf_types","pdf_types2")
-
-cl <- makeCluster( detectCores() %/% 2 )
-clusterEvalQ(cl, library(pdftools))
-system.time({
-  #result <- lapply(d2[["save_path"]], function(path) {
-  #  print(path)
-  result <- parLapply(cl, d2[["save_path"]], function(path) {
-    pdf_path <- file.path("..", path)
-    
-    # テキスト抽出
-    text <- pdf_text(pdf_path)
-    text <- text[text!=""]
-    tex2 <- text[1]
-    if(is.na(tex2)){
-      return(NA)
-    }
-    hed <- gsub(" ","", strsplit(tex2,"\n")[[1]][1])
-    tmp <- strsplit(tex2,"\n")[[1]]
-    tex3 <- tmp[1:min(c(length(tmp),20))]
-    
-    if(grepl("^公告",hed)){
-      return("公告")
-    }
-    
-    if(grepl("仕様書",hed)){
-      return("仕様書")
-    }
-    
-    if(any(tex3=="公告")){
-      return("公告")
-    }
-    
-    if(any(tex3=="変更公告")){
-      return("変更公告")
-    }
-    
-    if(any(grepl("情報・提案要求書",tex3))){
-      return("情報・提案要求書")
-    }
-    
-    if(grepl("競争参加者の資格に関する公示",hed)){
-      return("競争参加者の資格に関する公示")
-    }
-    
-    if(grepl("仕様書番号",hed)){
-      return("仕様書番号")
-    }
-    
-    if(grepl("公示.*号",hed)){
-      return("公示xx号")
-    }
-    
-    if(grepl("見積依頼",hed)){
-      return("見積依頼")
-    }
-    
-    if(grepl("^支担官第.*号",hed)){
-      return("支担官第xx号")
-    }
-    
-    if(any(grepl("業者の選定",tex3))){
-      return("業者の選定")
-    }
-    
-    return(hed)
+if(0){
+  pdfurl_subset_cols <- c("pdfUrl","pdf_types","pdf_types2")
+  
+  cl <- makeCluster( detectCores() %/% 2 )
+  clusterEvalQ(cl, library(pdftools))
+  system.time({
+    #result <- lapply(d2[["save_path"]], function(path) {
+    #  print(path)
+    result <- parLapply(cl, d2[["save_path"]], function(path) {
+      pdf_path <- file.path("..", path)
+      
+      # テキスト抽出
+      text <- pdf_text(pdf_path)
+      text <- text[text!=""]
+      tex2 <- text[1]
+      if(is.na(tex2)){
+        return(NA)
+      }
+      hed <- gsub(" ","", strsplit(tex2,"\n")[[1]][1])
+      tmp <- strsplit(tex2,"\n")[[1]]
+      tex3 <- tmp[1:min(c(length(tmp),20))]
+      
+      if(grepl("^公告",hed)){
+        return("公告")
+      }
+      
+      if(grepl("仕様書",hed)){
+        return("仕様書")
+      }
+      
+      if(any(tex3=="公告")){
+        return("公告")
+      }
+      
+      if(any(tex3=="変更公告")){
+        return("変更公告")
+      }
+      
+      if(any(grepl("情報・提案要求書",tex3))){
+        return("情報・提案要求書")
+      }
+      
+      if(grepl("競争参加者の資格に関する公示",hed)){
+        return("競争参加者の資格に関する公示")
+      }
+      
+      if(grepl("仕様書番号",hed)){
+        return("仕様書番号")
+      }
+      
+      if(grepl("公示.*号",hed)){
+        return("公示xx号")
+      }
+      
+      if(grepl("見積依頼",hed)){
+        return("見積依頼")
+      }
+      
+      if(grepl("^支担官第.*号",hed)){
+        return("支担官第xx号")
+      }
+      
+      if(any(grepl("業者の選定",tex3))){
+        return("業者の選定")
+      }
+      
+      return(hed)
+    })
   })
-})
-# 約45分。
-#ユーザ システム     経過 
-#  1.80     1.45  2058.28
-stopCluster(cl)
-
-d2[["pdf_types2"]] <- unlist(result)
-
-d1[["pdf_types2"]] <- d2[["pdf_types2"]][match(d1$pdfUrl,d2$pdfUrl)]
-d1[["pdf_types2"]][is.na(d1[["pdf_types2"]])] <- d1[["pdf_types"]][is.na(d1[["pdf_types2"]])]
-
-ag <- data.frame(table(d1[["pdf_types2"]]))
-ag <- ag[order(ag[["Freq"]],decreasing = T),]
-
-wh1 <- which(d1[["pdf_types2"]]=="競争参加者の資格に関する公示")
-wh1 <- which(d1[["pdf_types2"]]=="公告")
-d1[(wh1[length(wh1)]-3):(wh1[length(wh1)]+1),]
-
-d2 <- d1[d1$index!=1,]
-
-d3 <- d2[,pdfurl_subset_cols]
-d3[!grepl("入札公告",d3$pdf_types),]
-
+  # 約45分。
+  #ユーザ システム     経過 
+  #  1.80     1.45  2058.28
+  stopCluster(cl)
+  
+  d2[["pdf_types2"]] <- unlist(result)
+  
+  d1[["pdf_types2"]] <- d2[["pdf_types2"]][match(d1$pdfUrl,d2$pdfUrl)]
+  d1[["pdf_types2"]][is.na(d1[["pdf_types2"]])] <- d1[["pdf_types"]][is.na(d1[["pdf_types2"]])]
+  
+  ag <- data.frame(table(d1[["pdf_types2"]]))
+  ag <- ag[order(ag[["Freq"]],decreasing = T),]
+  
+  wh1 <- which(d1[["pdf_types2"]]=="競争参加者の資格に関する公示")
+  wh1 <- which(d1[["pdf_types2"]]=="公告")
+  d1[(wh1[length(wh1)]-3):(wh1[length(wh1)]+1),]
+  
+  d2 <- d1[d1$index!=1,]
+  
+  d3 <- d2[,pdfurl_subset_cols]
+  d3[!grepl("入札公告",d3$pdf_types),]
+}
 
 
 if(0){
